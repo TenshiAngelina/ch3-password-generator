da177c24996d4190e5859e54b05ac7e06766d5bb
TenshiAngelina <angelina.hcortes@gmail.com>
2023-05-23 13:43:19 -0600

    Correct functions

--- script.js
+++ script.js
@@ -1,49 +1,63 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector('#generate');
 var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 var numbers = [0,1,2,3,4,5,6,7,8,9];
 var characters = [' ','!','"','#','$','%','&','(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','[','^','_','`','{','|','}','~'];
-var generatedPass = [];
+var charLength = 8;
+// var password = generatePassword;
+
+// Add event listener to generate button
+generateBtn.addEventListener('click', writePassword);
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+  var prompts = userPrompts;
+  if(prompts) {
+    var password = generatePassword;
+    var passwordText = document.querySelector('#password');
+    passwordText.value = password;
+  }
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 
 function generatePassword(){
-  var password = "";
+  var password = '';
   for (var i = 0; i < charLength; i++){
-  var randomChar = Math.floor(Math.random() * generatedPass.length);
-  password = password + generatedPass[randomChar];
-}
-return password
+    var randomChar = Math.floor(Math.random() * generatedPass.length);
+    password = password + generatedPass[randomChar];
+  }
+  return password;
 }
 
 function userPrompts(){
-  generatedPass = [];
-  charLength = parseInt(prompt("How many characters (8 - 128)?"));
+  var generatedPass = [];
+  var charLength = parseInt(prompt('How many character do you want your password to be (choose a number between 8 and 128)?'));
+
   if(isNaN(charLength) || charLength < 8 || charLength > 128){
-    alert("You must enter a number for the length and 8 - 128 digits");
+    alert('You must enter a number between 8 and 128');
     return false;
   }
-  if (confirm("Lower case?")){
+
+  var confLCase = window.confirm('Include lower case?');
+  var confUCase = window.confirm('Include upper case?');
+  var confNumber = window.confirm('Include special characters?');
+  var confChara = window.confirm('Include numbers?');
+
+  if (confLCase){
     generatedPass = generatedPass.concat(lowerCase);
   }
-  if (confirm("Upper case?")){
+  if (confUCase){
     generatedPass = generatedPass.concat(upperCase);
   }
-  if (confirm("Special characters?")){
+  if (confNumber){
     generatedPass = generatedPass.concat(characters);
   }
-  if (confirm("Numbers?")){
+  if (confChara){
     generatedPass = generatedPass.concat(numbers);
+  }
+  if (generatedPass.length === 0) {
+    alert('You must select at least one type of character.');
+    return false;
   }
   return true;
 }
